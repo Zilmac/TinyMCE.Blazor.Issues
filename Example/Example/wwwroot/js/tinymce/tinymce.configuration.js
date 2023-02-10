c96ca54759c430c85bf0d46c7c0051c9dfa23b73
DEVB\zilvinas.macys <zilvinas.macys@devbridge.com>
2023-02-10 11:10:13 +0200

    Initial commit: TinyMCE RTE issue with list and value change

---
+++ Example/Example/wwwroot/js/tinymce/tinymce.configuration.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    window.exampleConfiguration = {
+        menubar: false,
+        plugins: 'lists link',
+        toolbar: 'bullist | fontfamily forecolor | bold underline | link',
+        branding: false,
+        automatic_uploads: false,
+        paste_data_images: false,
+        statusbar: false
+    };
+})();
